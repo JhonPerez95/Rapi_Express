49e3564e3190994acb6cae441d5a15dabd2d8683
jhonperez95 <jhonperez.j95@gmail.com>
2021-11-16 23:50:16 -0500

    agregando validaciones al formulario clientes

--- src/Controlador/CtrCliente.java
+++ src/Controlador/CtrCliente.java
@@ -1,5 +1,6 @@
 package Controlador;
 
+import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JOptionPane;
@@ -25,70 +26,71 @@ public class CtrCliente implements ActionListener {
         this.frmCliente.btnModificar.addActionListener(this);
     }
 
-    public void inicar() {
-        frmCliente.setTitle("Clientes");
-        frmCliente.setLocationRelativeTo(null);
-        frmCliente.setVisible(true);
-    }
-
-    public void ocultar() {
-        frmCliente.setVisible(false);
-    }
-
     @Override
     public void actionPerformed(ActionEvent e) {
+
+        // BOTON AGREGAR
         if (e.getSource() == frmCliente.btnAgregar) {
-            // Agregamos valores de los textos al modelo
 
-            client.setCedula_cliente(Integer.parseInt(frmCliente.txtCedula.getText()));
-            client.setNombre(frmCliente.txtNombre.getText());
-            client.setApellido(frmCliente.txtApellido.getText());
-            client.setCorreo(frmCliente.txtCorreo.getText());
-            client.setTelefono(frmCliente.txtTelefono.getText());
-            client.setDireccion(frmCliente.txtDireccion.getText());
+            if (validarFormulario()) {
+                client.setCedula_cliente(Integer.parseInt(frmCliente.txtCedula.getText()));
+                client.setNombre(frmCliente.txtNombre.getText());
+                client.setApellido(frmCliente.txtApellido.getText());
+                client.setCorreo(frmCliente.txtCorreo.getText());
+                client.setTelefono(frmCliente.txtTelefono.getText());
+                client.setDireccion(frmCliente.txtDireccion.getText());
 
-            if (consCliente.guardar(client)) {
-                limpiarTxt();
-                JOptionPane.showMessageDialog(null, "Registro Guardado");
+                if (consCliente.guardar(client)) {
+                    limpiarTxt();
+                    JOptionPane.showMessageDialog(null, "Registro Guardado");
+                } else {
+                    JOptionPane.showMessageDialog(null, "Error al  guardar");
+                }
             } else {
-//                limpiarTxt();
-                JOptionPane.showMessageDialog(null, "Error al  guardar");
+                JOptionPane.showMessageDialog(null, "Todos los campos son obligatorios por llenar !");
             }
+
         }
+
+        // BOTON CONSULTAR
         if (e.getSource() == frmCliente.btnConsultar) {
-            // Agregamos valores de los textos al modelo
-
             client.setCedula_cliente(Integer.parseInt(frmCliente.txtCedula.getText()));
-
             if (consCliente.traerUnCliente(client)) {
                 limpiarTxt();
                 JOptionPane.showMessageDialog(null, "Se trajeron Registros con exito");
             } else {
-                limpiarTxt();
-
                 JOptionPane.showMessageDialog(null, "Error al  traer registros");
             }
         }
 
+        // BOTON CANCELAR
         if (e.getSource() == frmCliente.btnCancelar) {
-
-            FrmInicial frmInicial = new FrmInicial();
-            CtrInicial ctrInicial = new CtrInicial(frmInicial);
             ocultar();
-            ctrInicial.mostrarFrame();
         }
-        if (e.getSource() == frmCliente.btnEliminar) {
-            client.setCedula_cliente(Integer.parseInt(frmCliente.txtCedula.getText()));
 
-            if (consCliente.eliminar(client)) {
-                limpiarTxt();
-                JOptionPane.showMessageDialog(null, "Registro Eliminado");
+        // BOTON ELIMINAR
+        if (e.getSource() == frmCliente.btnEliminar) {
+
+            String txtCedula = frmCliente.txtCedula.getText().trim();
+            System.out.println(txtCedula.length());
+
+            if (txtCedula.length() > 0) {
+                try {
+                    client.setCedula_cliente(Integer.parseInt(txtCedula));
+                    if (consCliente.eliminar(client)) {
+                        limpiarTxt();
+                        JOptionPane.showMessageDialog(null, "Registro Eliminado");
+                    } else {
+                        limpiarTxt();
+                        JOptionPane.showMessageDialog(null, "Error al  eliminar, comunicarse con el administrador !");
+                    }
+                } catch (NumberFormatException nfe) {
+                    JOptionPane.showMessageDialog(null, "El campo cedula debe ser un numero !");
+                }
             } else {
-                limpiarTxt();
-                JOptionPane.showMessageDialog(null, "Error al  eliminar");
+                JOptionPane.showMessageDialog(null, "Debe ingresar la cedula del cliente que quiere eliminar !");
             }
         }
-
     }
 
     public void limpiarTxt() {
@@ -98,5 +100,37 @@ public class CtrCliente implements ActionListener {
         frmCliente.txtCorreo.setText(null);
         frmCliente.txtDireccion.setText(null);
         frmCliente.txtTelefono.setText(null);
+    }
+
+    public boolean validarFormulario() {
+        if (frmCliente.txtCedula.getText().trim().length() == 0) {
+            return false;
+        }
+        if (frmCliente.txtNombre.getText().trim().length() == 0) {
+            return false;
+        }
+        if (frmCliente.txtApellido.getText().trim().length() == 0) {
+            return false;
+        }
+        if (frmCliente.txtCorreo.getText().trim().length() == 0) {
+            return false;
+        }
+        if (frmCliente.txtDireccion.getText().trim().length() == 0) {
+            return false;
+        }
+        if (frmCliente.txtTelefono.getText().trim().length() == 0) {
+            return false;
+        }
+        return true;
+    }
+
+    public void inicar() {
+        frmCliente.setTitle("Clientes");
+        frmCliente.setLocationRelativeTo(null);
+        frmCliente.setVisible(true);
+    }
+
+    public void ocultar() {
+        frmCliente.setVisible(false);
     }
 }
