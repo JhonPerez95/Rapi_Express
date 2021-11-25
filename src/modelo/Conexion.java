a6c33bc1a6f83f70c76047f8cf78bbcbeb93e1b1
Brayan <ing.brayan.villanueva@gmail.com>
2021-11-25 12:17:50 -0500

    se ordenan los botones en FrmInicial

--- src/modelo/Conexion.java
+++ src/modelo/Conexion.java
@@ -14,11 +14,13 @@ public class Conexion {
 
     String driver = "com.mysql.jdbc.Driver";
     String user = "root";
-    String pass = "root12345";    
-    //String pass = "root12345";
-
-    String nameDb = "bdcaferapiexpress";  
-    //String nameDb = "bdcaferapiexpress";
+    //clave perez
+   // String pass = "root12345";  
+    // clave villanueva
+    String pass = "";
+// BD perez
+    //String nameDb = "bdcaferapiexpress";  
+    String nameDb = "rapiexpress";
     String url = "jdbc:mysql://localhost:3306/" + nameDb + "?useUnicode=true&use"
             + "JDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&"
             + "serverTimezone=UTC";
@@ -28,8 +30,8 @@ public class Conexion {
 
     public Conexion() {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
             //Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName("com.mysql.jdbc.Driver");
 
             conn = DriverManager.getConnection(url, user, pass);
             if (conn != null) {
